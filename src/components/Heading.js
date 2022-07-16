5d943ebe720ac8d2f88e1b0a017bfc0b2edd148d
coldn00dles <93096256+coldn00dles@users.noreply.github.com>
2022-07-16 17:00:54 +0530

    int

---
+++ src/components/Heading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+function Heading(){
+    return (
+        <div className="head">
+            <h1>Snip-Snap : A URL Shortener</h1>
+        </div>
+    )
+    }
+export default Heading
